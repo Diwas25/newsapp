d18f7347fe6e27daca082742ce76aba2946e6542
Diwas Kumar Arya <diwasarya25@gmail.com>
2022-11-17 07:46:06 +0530

    creating structure

---
+++ src/components/NewsItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>
+        <h2>This is News Item Component</h2>
+      </div>
+    )
+  }
+}
+
+export default NewsItem
