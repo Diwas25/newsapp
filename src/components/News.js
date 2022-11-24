07b3fcf934e8918525233453484233dd4d83ec44
Diwas Kumar Arya <diwasarya25@gmail.com>
2022-11-24 08:48:02 +0530

    news content added

--- src/components/News.js
+++ src/components/News.js
@@ -2,15 +2,32 @@ import React, { Component } from 'react';
 import NewsItem from './NewsItem'; 
 
 export class News extends Component {
+  articles
+  constructor(){
+    super();
+    console.log("I am a constructor");
+    this.state = {
+
+    }
+  }
+
   render() {
     return (
-      <div>
-        <h1>This is a News Component</h1>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
+      <div className="container my-3">
+        <h2>NewsMonkey - Top Headlines</h2>
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description="myDescription" imageUrl="https://assets2.cbsnewsstatic.com/hub/i/r/2022/11/22/e74fda11-90e7-40bf-a197-eda981eb60ff/thumbnail/1200x630/7a70cb523f3c27c591e3bc0f42283967/ap22320158405104.jpg"/>
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description="myDescription"/>
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description="myDescription"/>
+          </div>
+        </div>
+       
+      
       </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+
   render() {
+    let {title, description, imageUrl} = this.props;
     return (
-      <div>
-        <h2>This is News Item Component</h2>
+      <div className="my-3">
+        <div className="card" style={{width: "18rem"}}>
+          <img src={imageUrl} className="card-img-top" alt="..."/>
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+          <a href=" " className="btn btn-sm btn-primary">Read More</a>
+      </div>
+      </div>
       </div>
     )
   }
